56f3a76a20cd8596711fc9db77d55cfbb9f72c47
norahkibathi <norah.njeri@student.moringaschool.com>
2024-04-05 23:19:19 +0300

    done

--- server/models.py
+++ server/models.py
@@ -7,3 +7,11 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake (db.Model, SerializerMixin):
+    __tablename__ = 'earthquakes'
+    id = db.Column(db.Integer, primary_key=True)
+    location= db.Column(db.String())
+    year =db.Column(db.Integer)
+    magnitude = db.Column(db.Float)
+    def __repr__(self):
+     return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
